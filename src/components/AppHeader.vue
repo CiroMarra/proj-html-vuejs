1aea3852372470280b87ecd94821683d7480792b
CiroMarra <tensha93@gmail.com>
2024-04-23 11:54:39 +0200

    feat: add navbar to header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,12 +6,9 @@
     <div class="bg-overlay"></div>
     <section>
       <div class="container">
-        <nav class="navbar navbar-expand-lg">
+        <nav class="navbar navbar-expand-lg ">
           <img src="../assets/img/menulogo.png" alt="">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <ul class="navbar me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+          <ul class="navbar me-auto my-2 my-lg-0 navbar-nav-scroll gap-5 mx-5" style="--bs-scroll-height: 100px;">
               <li class="nav-item dropdown ms-color">
                 <a class="nav-link dropdown-toggle ms-color" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   HOME
@@ -26,7 +23,60 @@
                   <li><a class="dropdown-item" href="#">Squad</a></li>
                 </ul>
               </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle ms-color text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  PAGES
+                </a>
+                <ul class="dropdown-menu ms-color">
+                  <li><a class="dropdown-item" href="#">About Us</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Matches</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Team</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Squad</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link  ms-color text-white" href="#">
+                  TOURNAMENT
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle ms-color text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  SHOP
+                </a>
+                <ul class="dropdown-menu ms-color">
+                  <li><a class="dropdown-item" href="#">About Us</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Matches</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Team</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Squad</a></li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle ms-color text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  BLOG
+                </a>
+                <ul class="dropdown-menu ms-color">
+                  <li><a class="dropdown-item" href="#">About Us</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Matches</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Team</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Squad</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link  ms-color text-white" href="#">
+                  CONTACT
+                </a>
+              </li>
           </ul>
+          
           <form class="d-flex" role="search">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success" type="submit">Search</button>
@@ -38,6 +88,9 @@
 </template>
 
 <style scoped>
+li {
+  list-style: none;
+}
 .ms-color {
   color: #05CC7C;
   list-style: none;
