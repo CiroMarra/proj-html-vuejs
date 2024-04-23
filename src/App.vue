ac3675815c1d946b3617899495c381ac4922e3a2
CiroMarra <tensha93@gmail.com>
2024-04-24 01:48:15 +0200

    feat: add a main

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-import AppHero from './components/AppHero.vue';
+import AppMain from './components/AppMain.vue';
 import './style/generic.sass';
 
 export default {
   components: {
     AppHeader,
-    AppHero
+    AppMain,
   }
 }
 </script>
@@ -16,7 +16,9 @@ export default {
     <header>
         <AppHeader></AppHeader>
     </header>
-    
+    <main>
+        <AppMain></AppMain>
+    </main>
 </template>
 
 <style scoped>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  name: 'AppMain',
+  data() {
+    return {
+    };
+  }
+}
+</script>
+
+<template>
+    <section class="ms-bg-section">
+        <div class="container position-relative">
+            <div class="box-img">
+                <img src="../assets/img/1.png" alt="" class="mx-4 py-3 px-2">
+                <img src="../assets/img/2.png" alt="" class="mx-4 py-3 px-2">
+                <img src="../assets/img/3.png" alt="" class="mx-4 py-3 px-2">
+                <img src="../assets/img/4.png" alt="" class="mx-4 py-3 px-2">
+                <img src="../assets/img/5.png" alt="" class="mx-4 py-3 px-2">
+                <img src="../assets/img/6.png" alt="" class="mx-4 py-3 px-2">
+            </div>
+        </div>
+    </section>
+
+</template>
+
+<style>
+ .ms-bg-section {
+    background-color: #191935;
+    height: 45vw;
+    color: white;
+ }
+
+ .box-img {
+    background-color: #202046;
+    height: 200px;
+    width: 1230px;
+    position: absolute;
+    bottom: -100px;
+    left: 40px;
+
+ }
+</style>
